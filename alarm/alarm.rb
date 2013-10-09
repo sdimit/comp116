a809292757205f9844a5ce036e70d752edafd07e
sdimit <Stefan@resnet161-221.medford.tufts.edu>
2013-10-08 23:14:53 -0400

    touch up

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -1,5 +1,4 @@
 require 'packetfu'
-require 'base64'
 include PacketFu
 
 $incident_number = 0
@@ -7,8 +6,8 @@ def alert(attack, pkt)
 	$incident_number+=1
 	puts "%3d. " % $incident_number + 
          "ALERT: #{attack} is detected from " + 
-         "%14s (#{pkt.proto.last}) !!!\n" % pkt.ip_saddr
-    puts "     packet to: %s, size: %s" % [pkt.ip_daddr, pkt.size ]#pkt.tcp_header.tcp_flags_readable]
+         "%14s (#{pkt.proto.last})!\n" % pkt.ip_saddr
+#    puts "     packet to: %s, size: %s" % [pkt.ip_daddr, pkt.size ]#pkt.tcp_header.tcp_flags_readable]
 end
 
 def checkForScans(pkt)
@@ -20,15 +19,15 @@ def checkForScans(pkt)
 	   	alert("XMAS scan", pkt)
 	when ".....F" # FIN is set
 	   	alert("FIN scan", pkt)
-	when "....S." # TODO: <insert source here>
+	when "....S." # discovered on http://danielmiessler.com/study/synpackets/
 			# nmap SYN packets dont have any of the fragmentation flags set
 			# such packets appear in SYN & other nmap scans like:
 			# Ping -sP, Version Detection -sV, UDP scans -sU, 
 			# IP protocol -sO, ACK -sA, Window -sW, RPC -sR
-			# looks like NMAP conveniently sends a SYN franken-packet
+			# NMAP conveniently sends a SYN franken-packet
 			# (no frag flags set) at the start of most scans
 		if (pkt.ip_header.ip_frag == 0)
-			alert("Nmap (SYN?) scan", pkt)
+			alert("Nmap scan", pkt)
 		end
 	else # check for *other* scans
 		if (tcpFlags.include? "SF") 	   # we have SYN & FIN - an atypical combo
@@ -62,7 +61,7 @@ def checkForLeaks(pkt)
    		alert("FTP password leaked", pkt) if (passmatch != nil)
    	when 23 
    	# We have a TELNET request. Will always leak logins in clear text
-   		alert("Login might leak! TELNET used)", pkt)
+   		alert("Login will leak! (TELNET was used)", pkt)
 	when 25 
 	# this is SMTP. keyword is AUTH LOGIN here
 		alert("SMTP login leaked", pkt) if (/AUTH LOGIN/i.match(pkt.payload)) 
@@ -72,7 +71,7 @@ def checkForLeaks(pkt)
 		if (/POST|GET|PUT/i =~ pkt.payload)
 			# this regex courtesy of http://stackoverflow.com/questions/9315647/regex-credit-card-number-tests
 			creditCardRegEx = /(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})/
-   			normalizedPayload = pkt.payload.gsub!(/-*\,*/, "")
+   			normalizedPayload = pkt.payload.gsub!(/-*\,*/, "") #removing dashes and commas from the payload
 	   		alert("Leaked credit card number", pkt) if (creditCardRegEx.match(normalizedPayload))
    		end
    		# check for password leaking over HTTP
@@ -81,28 +80,28 @@ def checkForLeaks(pkt)
 end
 
 def checkForXSS(pkt)
-   	# to check if we have a 
-#	if (/HTTP|GET|POST/.match(pkt.payload))
 		# ideas from here https://www.owasp.org/index.php/XSS_Filter_Evasion_Cheat_Sheet
 		# this to check if an attack vector has been converted to hexadecimal in the HTML format e.g. &#x0A 
-		# (there could be a trailing ; or not and we could have trailing zeroes too)
+		# (there could be a trailing ; or not and we could have trailing zeroes, too)
 		attackVectors = ["alert", "javascript", "window.location", "document.referrer", "document.location"]
 		attackVectors.map! { |evil| evil.each_byte.map { |b| sprintf("&#x[0]*%2X[;]*",b) }.join }
 		alert("XSS hexahide attack", pkt)	if (attackVectors.any? { |evil| /#{evil}/i.match(pkt.payload) })
-
 		# JS in sciprt tags # i at the end to ignore case.. 
 		test1 = /<script(.*src.*>|.*>.*(alert|window.location|document.referrer|document.location)).*/i
 		# JS embeded in img tags
 		test2 = /<img[ ]*(src=["`']*javascript:|onmouseover=["`']*)(alert|window.location|document.referrer|document.location)/i
 		# these can be used to fool filtering by 'breaking up' tags like javascript:, but browsers will ignore them.
+		# allegedly wasnt able to fool chrome with this while testing but the owasp article mentions it
 		test3 = /<(img|script)[ ]*src=["`']*.*(\t|&#x0A;|&#x09;|&#x0D;)/i 
 		alert("XSS attack", pkt) if (test1.match(pkt.payload) or test2.match(pkt.payload) or test3.match(pkt.payload))  
-#	end
-	#end
+end
 
-end #func end
 
-iface = 'en1'
+if ARGV.length == 1
+	iface = ARGV[0]
+else iface = 'en1'
+end
+
 cap = Capture.new(:start => true, :iface => iface, :promisc => true)
 packets = cap.stream
 
